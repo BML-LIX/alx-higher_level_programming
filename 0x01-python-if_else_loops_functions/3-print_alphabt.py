16dd48fab94cbf3991cea969ec6eda4121a59958
BML-LIX <atl.trait.all@gmail.com>
2022-09-21 05:46:44 +0300

    If/else functions tasks hot fix

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for cht in range(97, 123):
-        if cht != 101 and cht != 113:
-                print("{:c}".format(cht), end='')
+    if cht != 101 and cht != 113:
+        print("{:c}".format(cht), end='')
