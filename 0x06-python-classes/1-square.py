93a4838f03845cea9736f746d03cf3e3b3005963
BML-LIX <atl.trait.all@gmail.com>
2022-10-13 06:00:12 +0300

    Objects

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class Square:
+
+    def __init__(self, size):
+        """
+            Initialization of attributes for all instances
+            Args:
+                size (no type): size of the square
+        """
+
+        self.__size = size
